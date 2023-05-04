f11aecd336607f07511a90a8de0d4d05672c27df
webfarrock <webfarrock@mail.ru>
2023-05-04 06:51:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
