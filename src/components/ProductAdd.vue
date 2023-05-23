fa1ea4fa2fd1735e343a0b6c95e3f3a26294b631
pavel shcheglov <shcheglov_py@magnit.ru>
2023-05-23 08:55:38 +0300

    product add

--- src/components/ProductAdd.vue
+++ src/components/ProductAdd.vue
@@ -1,23 +1,101 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, minValue, minLength, url } from '@vuelidate/validators'
+
+const getClearFormData = () => ({
+  name: '',
+  category: '',
+  description: '',
+  price: null,
+  image: '',
+})
+const formData = ref(getClearFormData())
+const rules = {
+  name: { required, minLength: minLength(3) },
+  category: { required, minLength: minLength(3) },
+  description: { required, minLength: minLength(3) },
+  price: { required, minValue: minValue(1) },
+  image: { required, url },
+}
+const v$ = useVuelidate(rules, formData)
+
+const nameClassName = computed(() => ({
+  'is-valid': v$.value.$dirty && !v$.value.name.$error,
+  'is-invalid': v$.value.$dirty && v$.value.name.$error,
+}))
+
+const categoryClassName = computed(() => ({
+  'is-valid': v$.value.$dirty && !v$.value.category.$error,
+  'is-invalid': v$.value.$dirty && v$.value.category.$error,
+}))
+
+const descriptionClassName = computed(() => ({
+  'is-valid': v$.value.$dirty && !v$.value.description.$error,
+  'is-invalid': v$.value.$dirty && v$.value.description.$error,
+}))
+
+const priceClassName = computed(() => ({
+  'is-valid': v$.value.$dirty && !v$.value.price.$error,
+  'is-invalid': v$.value.$dirty && v$.value.price.$error,
+}))
+
+const imageClassName = computed(() => ({
+  'is-valid': v$.value.$dirty && !v$.value.image.$error,
+  'is-invalid': v$.value.$dirty && v$.value.image.$error,
+}))
+
+const btnClassNamae = computed(() => ({ disabled: v$.value.$dirty && v$.value.$error }))
+
+const clearForm = () => (formData.value = getClearFormData())
+const handleSubmit = async () => {
+  const isFormCorrect = await v$.value.$validate()
+  if (!isFormCorrect) {
+    console.log('from invalid')
+    return
+  }
+
+  alert(`Product ${formData.value.name} was added`)
+  console.log('form ok, data: ', formData.value)
+  clearForm()
+  v$.value.$reset()
+}
+</script>
 
 <template>
   <div class="py-5 text-center">
-    <h2>New product form</h2>
+    <h2>New product form {{ v$.$dirty }}</h2>
   </div>
 
-  <form>
+  <form novalidate @submit.prevent="handleSubmit">
     <div class="row py-1">
       <div class="col-12">
         <label class="form-label" for="product-name">Product name</label>
-        <input id="product-name" class="form-control" placeholder="" type="text" value="" />
-        <div class="invalid-feedback">Valid first name is required.</div>
+        <input
+          id="product-name"
+          v-model="formData.name"
+          :class="nameClassName"
+          class="form-control"
+          type="text"
+        />
+        <div class="invalid-feedback">
+          <div v-for="error of v$.name.$errors" :key="error.$uid">{{ error.$message }}</div>
+        </div>
       </div>
     </div>
     <div class="row py-1">
       <div class="col-12">
         <label class="form-label" for="category-name">Category name</label>
-        <input id="category-name" class="form-control" placeholder="" type="text" value="" />
-        <div class="invalid-feedback">Category name is required.</div>
+        <input
+          id="category-name"
+          v-model="formData.category"
+          :class="categoryClassName"
+          class="form-control"
+          type="text"
+        />
+        <div class="invalid-feedback">
+          <div v-for="error of v$.category.$errors" :key="error.$uid">{{ error.$message }}</div>
+        </div>
       </div>
     </div>
 
@@ -26,8 +104,16 @@
         <label class="form-label" for="price">Price</label>
         <div class="input-group has-validation">
           <span class="input-group-text">$</span>
-          <input id="price" class="form-control" placeholder="Price" type="text" />
-          <div class="invalid-feedback">Price is required.</div>
+          <input
+            id="price"
+            v-model="formData.price"
+            :class="priceClassName"
+            class="form-control"
+            type="number"
+          />
+          <div class="invalid-feedback">
+            <div v-for="error of v$.price.$errors" :key="error.$uid">{{ error.$message }}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -37,12 +123,14 @@
         <label class="form-label" for="description">Description</label>
         <input
           id="description"
+          v-model="formData.description"
+          :class="descriptionClassName"
           class="form-control"
-          placeholder="1234 Main St"
-          required
           type="text"
         />
-        <div class="invalid-feedback">Enter product description.</div>
+        <div class="invalid-feedback">
+          <div v-for="error of v$.description.$errors" :key="error.$uid">{{ error.$message }}</div>
+        </div>
       </div>
     </div>
 
@@ -51,17 +139,21 @@
         <label class="form-label" for="product-image">Image link</label>
         <input
           id="product-image"
+          v-model="formData.image"
+          :class="imageClassName"
           class="form-control"
-          placeholder="1234 Main St"
-          required
           type="text"
         />
-        <div class="invalid-feedback">Enter product description.</div>
+        <div class="invalid-feedback">
+          <div v-for="error of v$.image.$errors" :key="error.$uid">{{ error.$message }}</div>
+        </div>
       </div>
     </div>
 
     <hr class="my-4" />
 
-    <button class="w-100 btn btn-primary btn-lg" type="submit">Submit</button>
+    <button :class="btnClassNamae" class="w-100 btn btn-primary btn-lg" type="submit">
+      Submit
+    </button>
   </form>
 </template>
